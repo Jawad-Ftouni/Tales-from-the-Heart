e918315e0fbcc12a7278d29ddce61d2fc7d52c1f
Jawad-Ftouni <103638441+Jawad-Ftouni@users.noreply.github.com>
2023-03-12 01:42:09 +0200

    navbar and mainn search home components  added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { HeaderComponent } from 'src/components/header/header.component';
 import { HeaderTopComponent } from '../components/header-top/header-top.component';
+import { MainSearchFormComponent } from '../components/main-search-form/main-search-form.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeaderTopComponent } from '../components/header-top/header-top.componen
     NavbarComponent,
     HeaderComponent,
     HeaderTopComponent,
+    MainSearchFormComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ frontend/src/components/main-search-form/main-search-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-search-form',
+  templateUrl: './main-search-form.component.html',
+  styleUrls: [
+    './main-search-form.component.css',
+    '../../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+  ],
+})
+export class MainSearchFormComponent {}
