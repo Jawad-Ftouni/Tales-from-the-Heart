4f76d2c26c67a5c80c43f97bccd8341f0d341726
Jawad-Ftouni <103638441+Jawad-Ftouni@users.noreply.github.com>
2023-03-13 01:52:28 +0200

    initializing components to be builded and finishing designing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 import { HeaderComponent } from 'src/components/header/header.component';
 import { HeaderTopComponent } from '../components/header-top/header-top.component';
 import { MainSearchFormComponent } from '../components/main-search-form/main-search-form.component';
+import { FeaturedPostsComponent } from '../components/featured-posts/featured-posts.component';
+import { TravelComponent } from '../components/travel/travel.component';
+import { DestinationComponent } from '../components/destination/destination.component';
+import { FoodComponent } from '../components/food/food.component';
+import { HotelsComponent } from '../components/hotels/hotels.component';
+import { BlogComponent } from '../components/blog/blog.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +22,13 @@ import { MainSearchFormComponent } from '../components/main-search-form/main-sea
     HeaderComponent,
     HeaderTopComponent,
     MainSearchFormComponent,
+    FeaturedPostsComponent,
+    TravelComponent,
+    DestinationComponent,
+    FoodComponent,
+    HotelsComponent,
+    BlogComponent,
+    ContactComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
